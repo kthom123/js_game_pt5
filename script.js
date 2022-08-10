3864a3558add291fb21914451d167b94bb91cbcd
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-10 18:12:43 +0900

    added update and draw

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ class Raven {
     this.width = 100;
     this.height = 50;
     this.x = canvas.width;
-    this.y = Math.random() * canvas.height - this.height;
+    this.y = Math.random() * (canvas.height - this.height);
+    this.directionX = Math.random() * 5 + 3;
+    this.directionY = Math.random() * 5 - 2.5;
   }
+  update(){
+    this.x -= this.directionX;
+  }
+  draw(){
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function animate(timestamp){
+  ctx.clear
 }
