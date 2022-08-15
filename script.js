ad89a45163ca6344884258068284dbb85a44f8e4
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-15 15:06:03 +0900

    added score

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('canvas5');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let score = 0;
+ctx.font = '50px Impact';
 
 let timeToNextRaven = 0;
 let ravenInterval = 500;
@@ -24,17 +26,36 @@ class Raven {
     this.image.src = 'images/raven.png';
     this.frame = 0;
     this.maxFrame = 4;
+    this.timeSinceFlap = 0;
+    this.flapInterval = Math.random() * 50 + 50;
   }
-  update(){
+  update(deltatime){
+    if (this.y < 0 || this.y > canvas.height - this.height){
+      this.directionY = this.directionY * -1;
+    }
     this.x -= this.directionX;
+    this.y += this.directionY;
     if (this.x < 0 - this.width) this.markedForDeletion = true;
-    if (this.frame > this.maxFrame) this.frame = 0;
-    else this.frame++;
+    this.timeSinceFlap += deltatime;
+    if (this.timeSinceFlap > this.flapInterval){
+      if (this.frame > this.maxFrame) this.frame = 0;
+      else this.frame++;
+      this.timeSinceFlap = 0;
+    }
   }
   draw(){
     ctx.strokeRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.frame * this.spriteWidth, 0,
+      this.spriteWidth, this.spriteHeight, this.x, this.y, this.width,
+      this.height);
   }
+}
+
+function drawScore(){
+  ctx.fillStyle = 'black';
+  ctx.fillText('Score: ' + score, 50, 75);
+  ctx.fillStyle = 'white';
+  ctx.fillText('Score: ' + score, 55, 80);
 }
 
 function animate(timestamp){
@@ -46,7 +67,8 @@ function animate(timestamp){
     ravens.push(new Raven());
     timeToNextRaven = 0;
   };
-  [...ravens].forEach(object => object.update());
+  drawScore();
+  [...ravens].forEach(object => object.update(deltatime));
   [...ravens].forEach(object => object.draw());
   ravens = ravens.filter(object => !object.markedForDeletion);
   requestAnimationFrame(animate);
