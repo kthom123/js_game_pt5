4eeab24ddc4b52bee55cfb883df46dd2c4cf54fe
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-15 13:28:35 +0900

    added sprite image

--- script.js
+++ script.js
@@ -16,12 +16,19 @@ class Raven {
     this.y = Math.random() * (canvas.height - this.height);
     this.directionX = Math.random() * 5 + 3;
     this.directionY = Math.random() * 5 - 2.5;
+    this.markedForDeletion = false;
+    this.image = new Image();
+    this.image.src = 'raven.png';
+    this.spriteWidth = 271;
+    this.spriteHeight = 194;
   }
   update(){
     this.x -= this.directionX;
+    if (this.x < 0 - this.width) this.markedForDeletion = true;
   }
   draw(){
     ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x, this.y);
   }
 }
 
@@ -36,6 +43,7 @@ function animate(timestamp){
   };
   [...ravens].forEach(object => object.update());
   [...ravens].forEach(object => object.draw());
+  ravens = ravens.filter(object => !object.markedForDeletion);
   requestAnimationFrame(animate);
 }
 animate();
